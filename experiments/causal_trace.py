60a1cf764c280753194093a15d2c8b6d56582111
Yiran Shi <yiran0811@outlook.com>
2024-10-09 18:18:20 -0400

    Update causal_trace.py

--- experiments/causal_trace.py
+++ experiments/causal_trace.py
@@ -11,7 +11,7 @@ from datasets import load_dataset
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from dsets import ClinicalKnownsDataset
+from dsets.clinical_knowns import ClinicalKnownsDataset
 
 def main():
     parser = argparse.ArgumentParser(description="Causal Trace")
