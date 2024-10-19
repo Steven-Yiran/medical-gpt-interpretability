6dd587b6fd682464093174ca16ccf33209b6f938
yiran0811@outlook.com <Yiran Shi>
2024-10-19 23:59:31 +0000

    preprocess disease keywords

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Activate the virtual environment
-source /home/ubuntu/dev/medical-gpt-interpretability/ENV/bin/activate
-
-echo "Virtual environment has been activated."
-
 # Set the Git username and email
 git config --global user.name "yiran0811@outlook.com"
 git config --global user.email "Yiran Shi"
