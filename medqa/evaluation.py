550137337bd270c553bd01dc7149412dd9e373ba
Steven-Yiran <yiran0811@outlook.com>
2025-03-01 00:54:19 +0000

    meerkat results

--- medqa/evaluation.py
+++ medqa/evaluation.py
@@ -3,6 +3,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import json
 from datasets import load_dataset
 from prompts import prompt_eval_bare_fully
+import torch
+import re
+from tqdm import tqdm
+
+
+# Define prompt template
+prompt_template = """{question} (A) {choice_A} (B) {choice_B} (C) {choice_C} (D) {choice_D}"""
 
 def format_choices(choices):
     a = zip(list(choices.keys()), choices.values())
@@ -11,31 +18,53 @@ def format_choices(choices):
         final_answers.append(f'[{x}] : {y}')
     return "\n".join(final_answers)
 
+def check_answer(model, tokenizer, question, choices):
+    content = prompt_template.format(question=question, **choices)
+    content += " The answer is\n"
+    messages = [
+        {"role": "system", "content": "Answer the multiple-choice question about medical knowledge. Always answer in the form of [A], [B], [C], or [D].\n\n"},
+        {"role": "user", "content": content},
+    ]
+    encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda:0")
+    generated_ids = model.generate(encodeds, max_new_tokens=500, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+    decoded = tokenizer.batch_decode(generated_ids)
+
+    # Extract the generated response after "ASSISTANT"
+    assistant_index = decoded[0].find("ASSISTANT:")
+    if assistant_index != -1:
+        generated_response = decoded[0][assistant_index + len("ASSISTANT:"):].strip()
+    else:
+        generated_response = decoded[0].strip()
+    print(generated_response)
+    return generated_response
+
 
 def main():
     # Load the model and tokenizer
     model_name = "KrithikV/MedMobile"
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="cuda:0")
+    model_name = "dmis-lab/meerkat-7b-v1.0"
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="cuda:0")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    dataset = load_dataset("GBaker/MedQA-USMLE-4-options-hf", split="test")
-
+    dataset = load_dataset("GBaker/MedQA-USMLE-4-options-hf", split="test")    
+    # Create a 100 subset of the dataset
     results = []
-    for item in dataset:
+    for item in tqdm(dataset):
         question = item['sent1']
-        choices = {'A': item['ending0'], 'B': item['ending1'], 'C': item['ending2'], 'D': item['ending3']}
+        choices = {'choice_A': item['ending0'], 'choice_B': item['ending1'], 'choice_C': item['ending2'], 'choice_D': item['ending3']}
         correct_answer = item['label'] 
 
-        formatted_choices = format_choices(choices)
-        prompt = prompt_eval_bare_fully.format(question=question, choices=formatted_choices)
-        prompt += "## Answer"
+        generated_response = check_answer(model, tokenizer, question, choices)
         
-        inputs = tokenizer(prompt, return_tensors="pt").to("cuda:0")
-        outputs = model.generate(**inputs, max_length=2048)
-        generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        results.append({
+            "id": item['id'],
+            "gold": item['label'],
+            "generated_response": generated_response
+        })
 
-        print(generated_text)
-        break
+    model_name = model_name.split("/")[-1]
+    with open(f"{model_name}_results.json", "w") as f:
+        json.dump(results, f)
 
 if __name__ == "__main__":
     main()
---
+++ medqa/filter.py
@@ -0,0 +1,80 @@
+import json
+import re
+import sys
+import unicodedata
+
+class MultiChoiceFilter:
+    # Inspiring from lmeval
+    def __init__(self, ignore_case=False, ignore_punctuation=False, regex_pattern=r"[\(\[]([A-Z])[\)\]]"):
+        
+        self.ignore_case = ignore_case
+        self.ignore_punctuation = ignore_punctuation
+        self.regex_pattern = regex_pattern
+        self.regex = re.compile(regex_pattern)
+        self.punct_tbl = dict.fromkeys(i for i in range(sys.maxunicode) 
+                                       if unicodedata.category(chr(i)).startswith("P"))
+
+    def filter_text(self, text):
+        if self.ignore_case:
+            text = text.lower()
+        if self.ignore_punctuation:
+            text = text.translate(self.punct_tbl)
+        return text
+
+    def find_match(self, regex, resp, convert_dict={}):
+        match = regex.findall(resp)
+        if match:
+            match = match[-1]
+            if isinstance(match, tuple):
+                match = [m for m in match if m][0]
+            match = match.strip()
+            if match and match in convert_dict: 
+                match = convert_dict[match]
+        return match
+
+    def extract_answer(self, response, choices=None):
+        matchFirst = re.search(r'the answer is .(\w).', response)
+        if matchFirst:
+            return f"({matchFirst.group(1)})"
+        match = self.find_match(self.regex, response) 
+        if match:
+            return f"({match})"
+        return "[invalid]"
+
+    def filter_responses(self, responses, choices):
+        return [self.extract_answer(resp, choices) for resp in responses]
+
+
+def main():
+    model_name = "KrithikV/MedMobile"
+    model_name = model_name.split("/")[-1]
+    path = "meerkat-7b-v1.0_results.json"
+    with open(path, "r") as f:
+        results = json.load(f)
+
+    correct = 0
+    incorrect = 0
+
+    filter = MultiChoiceFilter()
+    choice_tally = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
+    for result in results:
+        generated_response = result["generated_response"]
+        answer = filter.extract_answer(generated_response)
+        gold = result["gold"]
+        answer = answer.replace("(", "").replace(")", "")
+
+        if answer not in ['A', 'B', 'C', 'D']:
+            incorrect += 1
+        else:
+            choice_tally[answer] += 1
+            answer_num = ord(answer) - ord('A')
+            if answer_num == gold:
+                correct += 1
+            else:
+                incorrect += 1
+
+    print(f"Correct: {correct}, Incorrect: {incorrect}")
+    print(f"Accuracy: {correct / (correct + incorrect)}")
+    print(f"Choice Tally: {choice_tally}")
+if __name__ == "__main__":
+    main()
